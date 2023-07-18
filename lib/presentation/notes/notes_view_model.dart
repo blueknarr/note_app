6f6b4065fd7b660e408fa0d3d678e23b17739e25
blueknarr <knarrblue@gmail.com>
2023-07-18 21:38:51 +0900

    Refactor: repository 제거 후 useCase 추가

--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -1,22 +1,20 @@
-import 'dart:collection';
-
 import 'package:flutter/cupertino.dart';
-import 'package:note_app/domain/repository/note_repository.dart';
 import 'package:note_app/presentation/notes/notes_state.dart';
 
 import '../../domain/model/note.dart';
+import '../../domain/use_case/use_cases.dart';
 import 'notes_event.dart';
 
 class NotesViewModel with ChangeNotifier {
-  final NoteRepository repository;
+  final UseCases useCases;
 
-  NotesState _state = NotesState();
+  NotesState _state = NotesState(notes: []);
 
   NotesState get state => _state;
 
   Note? _recentlyDeletedNote;
 
-  NotesViewModel(this.repository) {
+  NotesViewModel(this.useCases) {
     _loadNotes();
   }
 
@@ -29,13 +27,13 @@ class NotesViewModel with ChangeNotifier {
   }
 
   Future<void> _loadNotes() async {
-    List<Note> notes = await repository.getNotes();
+    List<Note> notes = await useCases.getNotes();
     _state = state.copyWith(notes: notes);
     notifyListeners();
   }
 
   Future<void> _deleteNote(Note note) async {
-    await repository.deleteNote(note);
+    await useCases.deleteNote(note);
 
     _recentlyDeletedNote = note;
 
@@ -44,7 +42,7 @@ class NotesViewModel with ChangeNotifier {
 
   Future<void> _restoreNote() async {
     if (_recentlyDeletedNote != null) {
-      await repository.insertNote(_recentlyDeletedNote!);
+      await useCases.addNote(_recentlyDeletedNote!);
       _recentlyDeletedNote = null;
       _loadNotes();
     }
