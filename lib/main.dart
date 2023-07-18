bf2437a5dd934732189b59e5fad1d50d6b07e50a
blueknarr <knarrblue@gmail.com>
2023-07-18 18:58:50 +0900

    Feat: DI 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/presentation/notes/notes_screen.dart';
+import 'package:note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'di/provider_setup.dart';
+
+void main() async {
+  // 플랫폼 채널의 위젯 바인딩을 보장
+  // 연결이 다 되는 것을 보장하게 해주는 코드
+  // 화면을 그리기 이전에 세팅하는 과정들을 보장하고 화면을 그린다.
+  // 카메라 세팅 라이브러리, 초기 future로 해야한다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final providers = await getProviders();
+  runApp(
+    MultiProvider(
+      providers: providers,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,10 +30,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.white,
+        canvasColor: darkGray,
+        floatingActionButtonTheme:
+            Theme.of(context).floatingActionButtonTheme.copyWith(
+                  backgroundColor: lightBlue,
+                  foregroundColor: darkGray,
+                ),
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              backgroundColor: darkGray,
+            ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const NotesScreen(),
     );
   }
 }
