a679b5d7fa794cd3dc81680eac44a1f82757ab00
blueknarr <knarrblue@gmail.com>
2023-07-18 21:38:23 +0900

    Refactor: UseCase 병합 및 이름 변경

--- lib/di/provider_setup.dart
+++ lib/di/provider_setup.dart
@@ -1,11 +1,18 @@
 import 'package:note_app/data/data_source/note_db_helper.dart';
 import 'package:note_app/data/repository/note_repository_impl.dart';
 import 'package:note_app/domain/repository/note_repository.dart';
+import 'package:note_app/domain/use_case/get_notes_use_case.dart';
+import 'package:note_app/domain/use_case/use_cases.dart';
 import 'package:note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
 import 'package:note_app/presentation/notes/notes_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:sqflite/sqflite.dart';
+
+import '../domain/use_case/add_note_use_case.dart';
+import '../domain/use_case/delete_note_use_case.dart';
+import '../domain/use_case/get_note_use_case.dart';
+import '../domain/use_case/update_note_use_case.dart';
 
 // DB는 App 시작 전에 시작해야함
 Future<List<SingleChildWidget>> getProviders() async {
@@ -17,7 +24,14 @@ Future<List<SingleChildWidget>> getProviders() async {
 
   NoteDbHelper noteDbHelper = NoteDbHelper(databse);
   NoteRepository repository = NoteRepositoryImpl(noteDbHelper);
-  NotesViewModel notesViewModel = NotesViewModel(repository);
+  UseCases useCases = UseCases(
+    addNote: AddNoteUseCase(repository),
+    deleteNote: DeleteNoteUseCase(repository),
+    getNote: GetNoteUseCase(repository),
+    getNotes: GetNotesUseCase(repository),
+    updateNote: UpdateNoteUseCase(repository),
+  );
+  NotesViewModel notesViewModel = NotesViewModel(useCases);
   AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository);
 
   return [
